7abaaee81f48a82dc19616916960ce746253a19e
Paty Reitman <97533555+Paty236@users.noreply.github.com>
2023-04-11 15:54:43 +0300

    Builder

---
+++ Builder/MessageBuilder.cs
@@ -0,0 +1,51 @@
+﻿public class Message
+{
+    public string Sender;
+    public List<string> Receivers = new List<string>();
+    public string Subject;
+    public string Body;
+
+    private Message() { }
+
+    public static MessageBuilder With()
+    {
+        return new MessageBuilder();
+    }
+
+    public class MessageBuilder
+    {
+        private readonly Message _message = new Message();
+
+        public MessageBuilder WithSender(string sender)
+        {
+            _message.Sender = sender;
+            return this;
+        }
+
+        public MessageBuilder WithReceiver(string receiver)
+        {
+            _message.Receivers.Add(receiver);
+            return this;
+        }
+
+        public MessageBuilder WithSubject(string subject)
+        {
+            _message.Subject = subject;
+            return this;
+        }
+
+        public MessageBuilder WithBody(string body)
+        {
+            _message.Body = body;
+            return this;
+        }
+
+        public Message Build()
+        {
+            if (string.IsNullOrEmpty(_message.Subject))
+                Console.WriteLine("Precis vrei sa transmiti mesajul fara subiect");
+
+            return _message;
+        }
+    }
+}
