93592948bdfbe0bc8ed88e8c6b88fff4bef52c87
Paty Reitman <97533555+Paty236@users.noreply.github.com>
2023-04-11 15:56:37 +0300

    Singleton

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+public class Singleton
+{
+    private static Singleton _instance;
+    private static readonly object _padlock = new object();
+
+    private Singleton()
+    {
+        
+    }
+
+    public static Singleton Instance()
+    {
+        if (_instance == null)
+        {
+            lock (_padlock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+            }
+        }
+        return _instance;
+    }
+
+    public void LogMsg(string logMsg)
+    {
+        lock (_padlock)
+        {
+            Console.WriteLine(logMsg);
+        }
+    }
+}
+
---
+++ Singleton/main.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Singleton.Instance().LogMsg("Message 1");
+
+        Thread thread1 = new Thread(new ThreadStart(ThreadMethod1));
+        Thread thread2 = new Thread(new ThreadStart(ThreadMethod2));
+
+        thread1.Start();
+        thread2.Start();
+
+        Singleton.Instance().LogMsg("Message 2");
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.ReadLine();
+    }
+
+    static void ThreadMethod1()
+    {
+        Singleton.Instance().LogMsg("Message 3 from Thread 1");
+    }
+
+    static void ThreadMethod2()
+    {
+        Singleton.Instance().LogMsg("Message 4 from Thread 2");
+    }
+}
