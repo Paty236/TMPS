a513f1b863322c017883390aac436c8b6ba4c3ab
Paty Reitman <97533555+Paty236@users.noreply.github.com>
2023-04-11 15:55:21 +0300

    Prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Obținem obiectul de la fabrică
+        IBaseClass obj = Factory.GetObject();
+
+        // Apelam metoda de imprimare pe obiect
+        obj.Print();
+
+        // Clonam obiectul și apelați metoda de imprimare pe clonă
+        IBaseClass objClone = obj.Clone();
+        objClone.Print();
+
+        // Așteptam ca utilizatorul să apese o tastă pentru a închide fereastra
+        Console.ReadKey();
+    }
+}
---
+++ Prototype/Prototype.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+abstract class IBaseClass
+        {
+            public abstract void Print();
+            public abstract IBaseClass Clone();
+        }
+
+        class DerivedOne : IBaseClass
+        {
+            public override void Print()
+            {
+                Console.WriteLine("Derived class one");
+            }
+
+            public override IBaseClass Clone()
+            {
+                return new DerivedOne();
+            }
+        }
+
+        class DerivedTwo : IBaseClass
+        {
+            public override void Print()
+            {
+                Console.WriteLine("Derived class two");
+            }
+
+            public override IBaseClass Clone()
+            {
+                return new DerivedTwo();
+            }
+        }
+
+        class Factory
+        {
+            public static IBaseClass GetObject()
+            {
+                Random random = new Random();
+                if (random.Next(2) == 0)
+                    return new DerivedOne();
+                else
+                    return new DerivedTwo();
+            }
+        }
