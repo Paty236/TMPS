1a7ee9f15049e336dcf7b5b6fae30ef732549d4b
Paty Reitman <97533555+Paty236@users.noreply.github.com>
2023-04-11 15:55:57 +0300

    Factory Method

---
+++ Factory Method/Class.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ExampleNamespace
+{
+    public interface ITheme
+    {
+        string GetColor();
+    }
+
+    public class LightTheme : ITheme
+    {
+        public string GetColor()
+        {
+            return "Light";
+        }
+    }
+
+    public class DarkTheme : ITheme
+    {
+        public string GetColor()
+        {
+            return "Dark";
+        }
+    }
+
+    public class Window
+    {
+        public Window(int x, int y)
+        {
+        }
+
+        public virtual ITheme GetUITheme()
+        {
+            return new DarkTheme();
+        }
+
+        public void Paint()
+        {
+            ITheme theme = GetUITheme();
+            Console.WriteLine($"Paint with {theme.GetColor()} color");
+        }
+    }
+
+    public class LightWindow : Window
+    {
+        public LightWindow(int x, int y) : base(x, y)
+        {
+        }
+
+        public override ITheme GetUITheme()
+        {
+            return new LightTheme();
+        }
+    }
+}
---
+++ Factory Method/Program.cs
@@ -0,0 +1,9 @@
+﻿using ExampleNamespace;
+
+Window darkWindow = new(10, 10);
+darkWindow.Paint();
+
+Window lightWindow = new LightWindow(20, 20);
+lightWindow.Paint();
+
+Console.ReadKey();
